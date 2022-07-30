e90004110e2d3da2bb7dc61f4defe3182eec43f2
Abdelrahman_Project <abdelrahman2490@yahoo.com>
2022-07-30 02:07:42 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,16 @@
+   
+
+  export const about = [
+    {
+      id:1,
+      logo:"SWIFTER SHOP",
+      disc:"There are many variations of passages of Lorem Ipsum available, but \n the majority have suffered alteration in some form, by injected\nhumour, or randomised words which don’t look even slightly believable.",
+      CD:"622 Dixie Path , South Tobinchester 98336",
+      num:"21313123124",
+      em:"sdkasodsk@yahoo.com"
+    },
+
+  ]
+
+  
+
